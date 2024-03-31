f37395f3df28cef587d7fa8d3293cb4b68f1658e
LUIS CR <>
2024-03-31 17:13:34 +0200

    0.9.5. Convertir la variable local "color" del método imprimir en un atributo.

--- src/figuras/Circunferencia.java
+++ src/figuras/Circunferencia.java
@@ -2,30 +2,31 @@ package figuras;
 
 public class Circunferencia {
 
-    public double getRadio()  {
-        return radio;
+    public double getRad()  {
+        return rad;
     }
 
-    public void setRadio(double radio) {
-        this.radio = radio;
+    public void setRad(double rad) {
+        this.rad = rad;
     }
-    private double radio;
+    private double rad;
 
     public Circunferencia(double radio) {
-        this.radio = radio;
+        this.rad = radio;
     }
 
     public void imprimir() {
-        String color = "rojo";
-        System.out.println("Di�metro: " + 2 * radio);
+        color = "rojo";
+        System.out.println("Di�metro: " + 2 * rad);
         System.out.println("Color: " + color);
-        double area = 2 * 3.1416 * radio * radio;
+        double area = 2 * 3.1416 * rad * rad;
         System.out.println(area);
     }
+    private String color;
 
     public boolean esIgual(Circunferencia otro, boolean conDecimales) {
-        double radio1 = this.radio;
-        double radio2 = otro.getRadio();
+        double radio1 = this.rad;
+        double radio2 = otro.getRad();
         if (conDecimales) {
             if (radio1 == radio2) {
                 return true;
